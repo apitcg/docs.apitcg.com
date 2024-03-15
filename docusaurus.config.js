c136a7fdba14059010feb1daeb3b3e05fe66f3c2
Sebastian Iturra <iturra.sebastian@hotmail.com>
2024-03-14 23:54:32 -0300

    Actualizar URL de edición de página

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -45,7 +45,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/apitcg/docs.apitcg.com',
         },
         theme: {
           customCss: './src/css/custom.css',
